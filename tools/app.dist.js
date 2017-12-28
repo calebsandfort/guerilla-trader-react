034e2ad00ff7f4c2c0924a728bbafe1108e0ba81
Caleb Sandfort <csandfort@isupport.com>
2017-12-28 08:08:39 -0800

    fix express call in app.dist.js

--- tools/app.dist.js
+++ tools/app.dist.js
@@ -5,7 +5,7 @@ const express = require('express'),
 /*eslint-disable no-console */
 
 const port = 8080;
-const app = express.createServer();
+const app = express();
 
 app.use(compression());
 app.use(express.static(__dirname));
