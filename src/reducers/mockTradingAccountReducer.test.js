9b4b871597a3d122fd0d2edd4ca37cbf8b16a8ba
Caleb Sandfort <csandfort@isupport.com>
2018-02-20 08:39:41 -0800

    fix build

--- src/reducers/mockTradingAccountReducer.test.js
+++ src/reducers/mockTradingAccountReducer.test.js
@@ -2,47 +2,47 @@ import expect from 'expect';
 import tradingAccountReducer from './mockTradingAccountReducer';
 import * as actions from '../actions/mockTradingAccountActions';
 
-// describe('Mock TradingAccount Reducer', () => {
-//   it('should add tradingAccount when passed MOCK_CREATE_TRADING_ACCOUNT_SUCCESS', () => {
-//     // arrange
-//     const initialState = [
-//       {Name: 'A'},
-//       {Name: 'B'}
-//     ];
-//
-//     const newTradingAccount = {Name: 'C'};
-//
-//     const action = actions.createTradingAccountSuccess(newTradingAccount);
-//
-//     // act
-//     const newState = tradingAccountReducer(initialState, action);
-//
-//     // assert
-//     expect(newState.length).toEqual(3);
-//     expect(newState[0].Name).toEqual('A');
-//     expect(newState[1].Name).toEqual('B');
-//     expect(newState[2].Name).toEqual('C');
-//   });
-//
-//   it('should update tradingAccount when passed MOCK_UPDATE_TRADING_ACCOUNT_SUCCESS', () => {
-//     // arrange
-//     const initialState = [
-//       {Id: 1, Name: 'A'},
-//       {Id: 2, Name: 'B'},
-//       {Id: 3, Name: 'C'}
-//     ];
-//
-//     const tradingAccount = {Id: 2, Name: 'New Title'};
-//     const action = actions.updateTradingAccountSuccess(tradingAccount);
-//
-//     // act
-//     const newState = tradingAccountReducer(initialState, action);
-//     const updatedTradingAccount = newState.find(a => a.Id == tradingAccount.Id);
-//     const untouchedTradingAccount = newState.find(a => a.Id == 1);
-//
-//     // assert
-//     expect(updatedTradingAccount.Name).toEqual('New Title');
-//     expect(untouchedTradingAccount.Name).toEqual('A');
-//     expect(newState.length).toEqual(3);
-//   });
-// });
+describe('Mock TradingAccount Reducer', () => {
+  it('should add tradingAccount when passed MOCK_CREATE_TRADING_ACCOUNT_SUCCESS', () => {
+    // arrange
+    const initialState = [
+      {Name: 'A'},
+      {Name: 'B'}
+    ];
+
+    const newTradingAccount = {Name: 'C'};
+
+    const action = actions.createTradingAccountSuccess(newTradingAccount);
+
+    // act
+    const newState = tradingAccountReducer(initialState, action);
+
+    // assert
+    expect(newState.length).toEqual(3);
+    expect(newState[0].Name).toEqual('A');
+    expect(newState[1].Name).toEqual('B');
+    expect(newState[2].Name).toEqual('C');
+  });
+
+  it('should update tradingAccount when passed MOCK_UPDATE_TRADING_ACCOUNT_SUCCESS', () => {
+    // arrange
+    const initialState = [
+      {Id: 1, Name: 'A'},
+      {Id: 2, Name: 'B'},
+      {Id: 3, Name: 'C'}
+    ];
+
+    const tradingAccount = {Id: 2, Name: 'New Title'};
+    const action = actions.updateTradingAccountSuccess(tradingAccount);
+
+    // act
+    const newState = tradingAccountReducer(initialState, action);
+    const updatedTradingAccount = newState.find(a => a.Id == tradingAccount.Id);
+    const untouchedTradingAccount = newState.find(a => a.Id == 1);
+
+    // assert
+    expect(updatedTradingAccount.Name).toEqual('New Title');
+    expect(untouchedTradingAccount.Name).toEqual('A');
+    expect(newState.length).toEqual(3);
+  });
+});
