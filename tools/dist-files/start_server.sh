aceff744165b1bed675dfd8807df39dfdc8084fc
Caleb Sandfort <csandfort@isupport.com>
2018-02-23 16:25:24 -0800

    try to get start_server.sh working

--- tools/dist-files/start_server.sh
+++ tools/dist-files/start_server.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 cd /home/ec2-user/app/api
 npm install
+sleep 5
 cd /home/ec2-user/app/socket-server
 npm install
+sleep 5
 forever start server.js
+sleep 5
 cd /home/ec2-user/app/
+sleep 5
 npm install
+sleep 5
 forever start app.js
 
