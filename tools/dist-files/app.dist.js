ba621c5afa3734c09473cc3ee19709f79250bafd
Caleb Sandfort <csandfort@isupport.com>
2018-03-06 14:55:06 -0800

    add bodyparser to app.js 2

--- tools/dist-files/app.dist.js
+++ tools/dist-files/app.dist.js
@@ -8,6 +8,9 @@ const express = require('express'),
 const port = 8080;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
